3dba5ccbbf781269938724d763cd2af3dd89839d
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-07-13 10:14:25 -0500

    Update server.py

--- v2/server.py
+++ v2/server.py
@@ -1,20 +1,45 @@
-from mesa.visualization.modules import CanvasGrid
-from mesa.visualization.ModularVisualization import ModularServer
+# ---------------------------------------------------------------- #
+# -- Imports Utils ------------------------------------------------ #
+# ---------------------------------------------------------------- #
+import sys
+import os
+sys.path.append(os.path.abspath("src"))
+# ---------------------------------------------------------------- #
+
+# ---------------------------------------------------------------- #
+# -- Imports MESA ------------------------------------------------ #
+# ---------------------------------------------------------------- #
+
 from mesa.visualization.UserParam import Slider
-from model import PalmerasModel
-from agents.PalmAgent import PalmAgent
-from agents.DroneAgent import DroneAgent
+from mesa.visualization.modules import CanvasGrid
+from mesa.datacollection import DataCollector
+from mesa.visualization.ModularVisualization import ModularServer
+
+# ---------------------------------------------------------------- #
+
+# ---------------------------------------------------------------- #
+# -- Imports Agents y Models ------------------------------------- #
+# ---------------------------------------------------------------- #
+
+# Agents
+from src.agents.PalmAgent import PalmAgent
+from src.agents.DroneAgent import DroneAgent
+
+# Models
+from src.models.model import PalmerasModel
+# ---------------------------------------------------------------- #
+
 def agent_portrayal(agent):
     portrayal = {"Layer": 0, "w": 1, "h": 1}
     
     if isinstance(agent, PalmAgent):
         if agent.estado == "verde":
-            portrayal["Shape"] = "images/palmera_verde.png"
+            portrayal["Shape"] = "images/palm_green.png"
         elif agent.estado == "infectada":
-            portrayal["Shape"] = "images/palmera_cafe.png"
+            portrayal["Shape"] = "images/palm_brown.png"
     elif isinstance(agent, DroneAgent):
         portrayal = {
-            "Shape": "images/dron.png",
+            "Shape": "images/drone.png",
             "Layer": 1,
             "w": 1,
             "h": 1,
@@ -22,13 +47,17 @@ def agent_portrayal(agent):
     
     return portrayal
 
+
 grid = CanvasGrid(agent_portrayal, 20, 20, 600, 600)
 
 model_params = {
+
     "width": 20,
     "height": 20,
+
     "densidad": Slider("Densidad de palmeras", 0.6, 0.1, 1.0, 0.1),
     "n_drones": Slider("Número de drones", 3, 1, 10, 1),
+
     "tasa_propagacion": Slider("Tasa de propagación", 0.2, 0.0, 1.0, 0.05),
     "tasa_cura": Slider("Tasa de curación por drones", 1.0, 0.0, 1.0, 0.05),
 }
@@ -41,6 +70,3 @@ server = ModularServer(
 )
 server.port = 8560
 server.launch()
-
-
-
