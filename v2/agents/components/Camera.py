930867fb5aac288cbeccd3e7b117a30d54558896
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-07-13 10:14:59 -0500

    Delete Camera.py

--- v2/agents/components/Camera.py
+++
@@ -1,8 +0,0 @@
-class Camera:
-    def __init__(self):
-        self.last_photo = None
-
-    def take_photo(self):
-        # Dummy placeholder, would return an image or ID
-        self.last_photo = "photo_data"
-        return self.last_photo
