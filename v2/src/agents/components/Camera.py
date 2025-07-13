b1c79b251f5f4014b6c4baaed7ef02d3264a51cf
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-07-13 10:14:16 -0500

    Create Camera.py

---
+++ v2/src/agents/components/Camera.py
@@ -0,0 +1,8 @@
+class Camera:
+    def __init__(self, model):
+        self.last_photo = None
+
+    def take_photo(self):
+        # Dummy placeholder, would return an image or ID
+        self.last_photo = "photo_data"
+        return self.last_photo
